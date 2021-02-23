a32a5cc17364428d0ecf09466a63d54879c3fa47
matteoem <matteoemanuele0@gmail.com>
2021-02-23 08:25:50 +0100

    Update chart.js

--- chart.js
+++ chart.js
@@ -263,61 +263,6 @@ function create_boxplot(svg,margin1,data_final,attr,color){
 //--------------------------------------------------------------------------------------------------------------------
 
 
-
-// function create_scatterplot(svg,margin){
-
-//     // set the dimensions and margins of the graph
-//     width = 460 - margin.left - margin.right,
-//     height = 400 - margin.top - margin.bottom;
-
-//     // append the svg object to the body of the page
-//     var svg = d3.select("#my_dataviz_boxplot")
-//     .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//     .append("g")
-//     .attr("transform",
-//         "translate(" + margin.left + "," + margin.top + ")");
-
-//     //Read the data
-//     d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv", function(data) {
-
-//     print(data)
-
-//     // Add X axis
-//     var x = d3.scaleLinear()
-//     .domain([0, 4000])
-//     .range([ 0, width ]);
-//     svg.append("g")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(x));
-
-//     // Add Y axis
-//     var y = d3.scaleLinear()
-//     .domain([0, 500000])
-//     .range([ height, 0]);
-//     svg.append("g")
-//     .call(d3.axisLeft(y));
-
-//     // Add dots
-//     svg.append('g')
-//     .selectAll("dot")
-//     .data(data)
-//     .enter()
-//     .append("circle")
-//     .attr("cx", function (d) { return x(d.GrLivArea); } )
-//     .attr("cy", function (d) { return y(d.SalePrice); } )
-//     .attr("r", 1.5)
-//     .style("fill", "#69b3a2")
-
-//     })
-// }
-
-
-//create_scatterplot(svg,margin1)
-// var already_draw = false
-// var single_chart = false
-
 function createGraphsOfMyCrypto(name1,name2='Dogecoin'){
     //PER ORA IL CONFRONTO E' FRA QUELLA CHE CLICCO,E Dogecoin.
     already_draw=true
